ab1d1d663180d6bbc812eea3cfc187ad97be3738
Haixiang <haixiang6123@gmail.com>
2020-05-15 14:09:08 -0700

    添加记账页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import styled from 'styled-components'
+import Summary from './pages/Summary'
+
+const StyledApp = styled.div`
+  max-width: 540px;
+  margin: 0 auto;
+  background: #EDEDED;
+  height: 100vh;
+`
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      App
-    </div>
+    <StyledApp>
+      <Summary/>
+    </StyledApp>
   );
 }
 
---
+++ src/pages/Summary.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const Summary: React.FC = () => {
+  return (
+    <div>记账页</div>
+  )
+}
+
+export default Summary
