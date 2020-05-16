5769028b0281877365160c3c165efa4ce7106447
Haixiang <haixiang6123@gmail.com>
2020-05-16 09:47:12 -0700

    添加 MonthRecord 的 Header

--- src/components/MonthRecord.tsx
+++ src/components/MonthRecord.tsx
@@ -2,23 +2,38 @@ import * as React from 'react'
 import DayRecord from './DayRecord'
 import {TMonthRecord} from '../hooks/useRecordList'
 import styled from 'styled-components'
+import Divider from './Dividier'
 
 type TProps = TMonthRecord & {}
 
-const RecordList = styled.ul`
-  margin-bottom: 8px;
+const Header = styled.div`
+  padding: 8px 18px;
+  span {
+    font-size: ${props => props.theme.$normalTextSize};
+    color: ${props => props.theme.$subText}
+  }
 `
+
+const RecordList = styled.li``
 
 const MonthRecord: React.FC<TProps> = (props) => {
   const {month, recordList} = props
 
   return (
     <RecordList>
-      {
-        recordList && recordList.map(dayRecord => (
-          <DayRecord key={dayRecord.day} {...dayRecord}/>
-        ))
-      }
+      <Header>
+        <span>{month}</span>
+        <Divider gap={8}/>
+        <span>总支出￥xxx</span>
+        <span>总收入￥yyy</span>
+      </Header>
+      <ul>
+        {
+          recordList && recordList.map(dayRecord => (
+            <DayRecord key={dayRecord.day} {...dayRecord}/>
+          ))
+        }
+      </ul>
     </RecordList>
   )
 }
