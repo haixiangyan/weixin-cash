62a2b2e6da402893528268292a7d72e31752ffe9
Haixiang <haixiang6123@gmail.com>
2020-05-17 13:50:03 -0700

    规定 Sticker 长宽

--- src/components/Sticker.tsx
+++ src/components/Sticker.tsx
@@ -2,11 +2,15 @@ import * as React from 'react'
 import styled from 'styled-components'
 
 const StyledSticker = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   position: absolute;
   bottom: 128px;
   right: 32px;
+  width: 50px;
+  height: 50px;
   background: white;
-  padding: 12px;
   box-shadow: 0 2px 24px -6px ${props => props.theme.$normalText};
   z-index: 3;
   border-radius: 50%;
