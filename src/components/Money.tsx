7d39a1478b5c55455e0c204a9b3887784b86e804
Haixiang <haixiang6123@gmail.com>
2020-05-19 11:42:40 -0700

    封装 Button 组件

--- src/components/Money.tsx
+++ src/components/Money.tsx
@@ -3,27 +3,39 @@ import styled from 'styled-components'
 import Icon from './Icon'
 import Category from './Category'
 import {DEFAULT_CATEGORIES} from '../lib/category'
+import {useState} from 'react'
+import {TRecordType} from '../hooks/useRecordList'
+import Button from './Button'
 
 const Header = styled.header`
-  
+  padding: 16px;
 `
 
 const TypeSection = styled.section`
-  
+  padding: 16px 24px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `
 
 const Money: React.FC = () => {
+  const [type] = useState<TRecordType>('income')
   const category = DEFAULT_CATEGORIES[0]
 
   return (
     <div>
       <Header>
-        <Icon name="cancel" size={32}/>
+        <Icon name="cancel" size={18}/>
       </Header>
       <TypeSection>
-          <button>支出</button>
-          <button>收入</button>
-          <button>5月15日</button>
+        <div>
+          <Button type={type === 'expense' ? 'success' : 'none'}>支出</Button>
+          <Button type={type === 'income' ? 'warning': 'none'}>收入</Button>
+        </div>
+        <Button>
+          <span style={{marginRight: 4}}>5月5号</span>
+          <Icon name="dropdown" size={8}/>
+        </Button>
       </TypeSection>
       <section>
         <input type="text"/>
