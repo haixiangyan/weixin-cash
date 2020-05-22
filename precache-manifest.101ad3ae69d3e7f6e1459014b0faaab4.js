384076994b3d54d34144251a41c2cb240cfe17e4
Haixiang <haixiang6123@gmail.com>
2020-05-22 21:02:34 +0800

    Updates

---
+++ precache-manifest.101ad3ae69d3e7f6e1459014b0faaab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57cc76c02ec8b652e25f330b8fe3032b",
+    "url": "/weixin-cash/index.html"
+  },
+  {
+    "revision": "5843b491545e71b4f085",
+    "url": "/weixin-cash/static/css/main.810302d4.chunk.css"
+  },
+  {
+    "revision": "c7ff06e039280e65577a",
+    "url": "/weixin-cash/static/js/2.24eda5c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weixin-cash/static/js/2.24eda5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5843b491545e71b4f085",
+    "url": "/weixin-cash/static/js/main.c9f85143.chunk.js"
+  },
+  {
+    "revision": "b582b2fea3c2a817e8e2",
+    "url": "/weixin-cash/static/js/runtime-main.6c4d1b1a.js"
+  }
+]);
