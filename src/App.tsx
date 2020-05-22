d11771a410ca4281aece153a16e9d86aaee87ab9
Haixiang <haixiang6123@gmail.com>
2020-05-22 20:53:23 +0800

    使用 HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled, {ThemeProvider} from 'styled-components'
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
