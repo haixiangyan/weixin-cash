3956e48df00c187cc4402e1528a07b328d224050
Haixiang <haixiang6123@gmail.com>
2020-05-22 20:28:37 +0800

    添加二维码

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import Layout from '../components/Layout'
 import styled from 'styled-components'
 import Icon from '../components/Icon'
+import Drawer from '../components/Drawer'
+import {useState} from 'react'
 
 const Main = styled.div`
   flex-grow: 1;
@@ -22,11 +24,25 @@ const Item = styled.div`
   }
 `
 
+const QrCode = styled.div`
+  padding: 32px;
+  text-align: center;
+  > p {
+    font-size: ${props => props.theme.$largeTextSize};
+  }
+  > img {
+    margin-top: 24px;
+    width: 200px;
+  }
+`
+
 const Settings: React.FC = () => {
+  const [qrcode, toggleQrcode] = useState(false)
+
   return (
     <Layout>
       <Main>
-        <Item style={{marginBottom: 8}}>
+        <Item style={{marginBottom: 8}} onClick={() => toggleQrcode(true)}>
           <Icon name="qrcode"/>
           <span>把记账本推荐给朋友</span>
         </Item>
@@ -41,6 +57,13 @@ const Settings: React.FC = () => {
           <span>联系我</span>
         </Item>
       </Main>
+
+      <Drawer show={qrcode} closeDrawer={() => toggleQrcode(false)}>
+        <QrCode>
+          <p>扫一下面二维码以分享该记账本</p>
+          <img src={require('../assets/img/qrcode.png')} alt=""/>
+        </QrCode>
+      </Drawer>
     </Layout>
   )
 }
